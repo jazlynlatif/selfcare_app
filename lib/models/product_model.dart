9e6cd0200a39c69c920f4b77afd969285a02608c
Jazlyn Latif <jazlyn.jkl@gmail.com>
2026-03-30 19:44:50 +0700

    refactor: rename product model fields

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -3,17 +3,17 @@ import 'package:flutter/foundation.dart';
 class Product {
   final int id;
   final int categoryId;
-  final String productName;
-  final double productSize;
-  final int productSizeUnitId;
-  final int productTypeId;
+  final String name;
+  final double size;
+  final int sizeUnitId;
+  final int typeId;
 
   Product({
     required this.id,
     required this.categoryId,
-    required this.productName,
-    required this.productSize,
-    required this.productSizeUnitId,
-    required this.productTypeId
+    required this.name,
+    required this.size,
+    required this.sizeUnitId,
+    required this.typeId
   });
 }
