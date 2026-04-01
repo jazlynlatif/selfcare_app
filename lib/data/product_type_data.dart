259a8ad9d7f45dfb61c8fa8ff0ab5f48b4f23f0f
Jazlyn Latif <jazlyn.jkl@gmail.com>
2026-04-01 16:25:55 +0700

    feat: add product type data

---
+++ lib/data/product_type_data.dart
@@ -0,0 +1,8 @@
+import 'package:selfcare_app/models/product_type_model.dart';
+
+final List<ProductType> productTypeData = [
+  ProductType(id: 1, categoryId: 1, name: 'Toner'),
+  ProductType(id: 2, categoryId: 1, name: 'Moisturizer'),
+  ProductType(id: 3, categoryId: 1, name: 'Facial Wash'),
+  ProductType(id: 4, categoryId: 1, name: 'Lip Balm')
+];
