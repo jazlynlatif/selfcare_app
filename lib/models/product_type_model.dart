6f70a015bc0197abc0de5db821093f9088ad41d1
Jazlyn Latif <jazlyn.jkl@gmail.com>
2026-04-01 16:20:55 +0700

    feat: add product type model

---
+++ lib/models/product_type_model.dart
@@ -0,0 +1,11 @@
+class ProductType{
+  final int id;
+  final int categoryId;
+  final String name;
+
+  ProductType({
+    required this.id,
+    required this.categoryId,
+    required this.name
+  });
+}
