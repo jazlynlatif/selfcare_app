f53ee66ebfcdb45faef14c44c3c3255b348014cb
Jazlyn Latif <jazlyn.jkl@gmail.com>
2026-04-01 16:03:54 +0700

    feat: add product data

---
+++ lib/data/product_data.dart
@@ -0,0 +1,8 @@
+import 'package:selfcare_app/models/product_model.dart';
+
+final List<Product> productData = [
+  Product(id: 1, categoryId: 1, name: 'Pyunkang Yul Mist Toner', size: 200, sizeUnitId: 1, typeId: 1),
+  Product(id: 2, categoryId: 1, name: 'Innisfree Vitamin C Green Tea Enzyme Brightening Cream', size: 50, sizeUnitId: 1, typeId: 2),
+  Product(id: 3, categoryId: 1, name: 'SENKA Perfect Whip U Facial Foam', size: 120, sizeUnitId: 2, typeId: 3),
+  Product(id: 4, categoryId: 1, name: 'Mediheal Labocare Pantenolips Healssence', size: 10, sizeUnitId: 1, typeId: 4)
+];
